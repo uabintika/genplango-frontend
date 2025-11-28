11c7006dc533384affa07d5d2abfc92e2e1c59fa
simonasbabar <simonas.babarskas@popit.lt>
2025-11-28 13:53:43 +0200

    GPG-3: added link to create SR

--- app/(protected)/admin/service-recipients/_components/table-filter.tsx
+++ app/(protected)/admin/service-recipients/_components/table-filter.tsx
@@ -6,8 +6,11 @@ import {
   InputGroupAddon,
   InputGroupInput,
 } from "@/components/ui/input-group";
-import { SearchIcon } from "lucide-react";
+import { PlusCircle, SearchIcon } from "lucide-react";
 import useDebounce from "@/hooks/use-debounce";
+import Link from "next/link";
+import { ROUTES } from "@/routes";
+import { Button } from "@/components/ui/button";
 
 type TableFiltersProps = {
   isLoading: boolean;
@@ -32,18 +35,28 @@ export default function ServiceRecipientTableFilters({
   const debounceSearch = useDebounce(onInputChange, 500);
 
   return (
-    <div className="p-5 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-3">
-      <InputGroup>
-        <InputGroupInput
-          name="search"
-          placeholder={t("search")}
-          disabled={isLoading || isValidating}
-          onChange={debounceSearch}
-        />
-        <InputGroupAddon align="inline-end">
-          <SearchIcon />
-        </InputGroupAddon>
-      </InputGroup>
-    </div>
+    <>
+      <div className="p-5 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-3">
+        <InputGroup>
+          <InputGroupInput
+            name="search"
+            placeholder={t("search")}
+            disabled={isLoading || isValidating}
+            onChange={debounceSearch}
+          />
+          <InputGroupAddon align="inline-end">
+            <SearchIcon />
+          </InputGroupAddon>
+        </InputGroup>
+      </div>
+      <div className="px-5 pb-5">
+        <Button asChild color="success" size="md">
+          <Link href={ROUTES.ADMIN.SERVICE_RECIPIENTS.CREATE}>
+            <span>Sukurti klientą</span>
+            <PlusCircle className="ms-1" />
+          </Link>
+        </Button>
+      </div>
+    </>
   );
 }
